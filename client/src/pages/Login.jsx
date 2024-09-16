b0ba656e31f03d83b0ef124e293417b45ef0256a
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-16 13:39:20 +0800

    Subject:Body

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,13 +3,14 @@ import { LoginCard } from '../components/Login Cards/LoginCard';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import { NavbarLogin } from '../components/Navbar/NavbarLogin';
 
 export const Login = () => {
 
     return (
         <>
             <Container fluid>
-
+                <NavbarLogin/>
             </Container>
 
             <Container lg>
